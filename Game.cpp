56f9256421a96d9f6116a4e460d53bafca55d7e7
Seif Allah Haytham <S.Haytham2483@nu.edu.eg>
2025-01-23 01:10:01 +0200

    Fix drawBoard function
    drawBoard function was not drawing the board correctly.
    Fixed the logic to draw the board.
    Enhanced the output of drawBoard.

--- Game.cpp
+++ Game.cpp
@@ -9,35 +9,36 @@ bool checkForTie(void);
 
 int main(void)
 {
-
+    drawBoard();
     return 0;
 }
 
 void drawBoard(void) {
     char board[3][3] = { {'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'} };
-    std::cout << " 1 | 2 | 3\n";
+    std::cout << "Current board:\n";
 
-    for (int i = 0; i < 3; i++) {
-        std::cout << i + 1 << " ";
-        
-        for (int j = 0; j < 3; j++) {
-            std::cout << board[i][j];
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            std::cout << " " << board[i][j] << " ";
+            if (j < 2) std::cout << "|";
         }
+        std::cout << "\n";
+        if (i < 2) std::cout << "---|---|---\n";
     }
 }
 
 bool placeMarker(int slot) {
-
+    
 }
 
 void switchPlayer(void) {
-
+    
 }
 
 bool checkForWin(void) {
-
+    
 }
 
 bool checkForTie(void) {
-
+    
 }
