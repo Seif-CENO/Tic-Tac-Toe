6d4609b46d52284a0535bb6bda1cbf250cdfb584
Seif Allah Haytham <S.Haytham2483@nu.edu.eg>
2025-01-23 00:57:36 +0200

    Add drawBoard function
    Implement the drawBoard function to display the board in the console.
    This function will be used to display the board after each move,
    providing a clear and updated view of the game.

--- Game.cpp
+++ Game.cpp
@@ -14,7 +14,16 @@ int main(void)
 }
 
 void drawBoard(void) {
-    
+    char board[3][3] = { {'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'} };
+    std::cout << " 1 | 2 | 3\n";
+
+    for (int i = 0; i < 3; i++) {
+        std::cout << i + 1 << " ";
+        
+        for (int j = 0; j < 3; j++) {
+            std::cout << board[i][j];
+        }
+    }
 }
 
 bool placeMarker(int slot) {
