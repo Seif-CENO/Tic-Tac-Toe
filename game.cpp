8f94c1870569daefb9d061dd395e70b356b285d9
Seif Allah Haytham <S.Haytham2483@nu.edu.eg>
2025-01-23 03:20:40 +0200

    Add checkForTie function
    Implements 2 nested for loops: checks
    all board positions if they are not empty.
    Returns true if all positions are filled.

--- game.cpp
+++ game.cpp
@@ -38,3 +38,14 @@ bool checkForWin(char board[3][3], char marker) {
 
     return false; // No win yet
 }
+
+bool checkForTie(char board[3][3]) {
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            if (board[i][j] == ' ') {
+                return false; // Moves are still available
+            }
+        }
+    }
+    return true; // No moves left aka. it's tie
+}
