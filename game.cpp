8b9bac662753a940cd8ae959a6c0497cc94719ac
Seif Allah Haytham <S.Haytham2483@nu.edu.eg>
2025-01-23 03:08:22 +0200

    Add drawBoard function & game.h header
    drawBoard function draws the board for display.
    game.h header file contains the function prototypes.

---
+++ game.cpp
@@ -0,0 +1,14 @@
+#include "game.h"
+
+void drawBoard(char board[3][3]) {
+    std::cout << "Current board:\n";
+
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            std::cout << " " << board[i][j] << " ";
+            if (j < 2) std::cout << "|";
+        }
+        std::cout << "\n";
+        if (i < 2) std::cout << "---|---|---\n";
+    }
+}
